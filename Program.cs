ebeba1976d20103b47a3b6f6a6536a83675e7563
PedroAMF <pedroamf@gmail.com>
2020-06-08 22:20:33 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CalcGithub {
+    class Program {
+        static void Main (string[] args) {
+            int num1;
+            int num2;
+            int soma;
+            Console.WriteLine ("Digite o primeiro numero:");
+            num1 = int.Parse (Console.ReadLine ());
+            Console.WriteLine ("Digite o segundo numero:");
+            num2 = int.Parse (Console.ReadLine ());
+            soma = num1 + num2;
+
+            Console.WriteLine (soma);
+        }
+    }
+}
