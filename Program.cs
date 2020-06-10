e456a8a5843c5a95c2f222a59ab0d0e3a11bb760
PedroAMF <pedroamf@gmail.com>
2020-06-10 13:12:09 +0100

    Melhorado a estrutura de escolha
    Com a ajuda do https://www.linkedin.com/in/luisdeol/ fizemos o loop do programa e a opção do usuário sair do programa.

--- Program.cs
+++ Program.cs
@@ -7,59 +7,70 @@ namespace CalcGithub {
             float num1;
             float num2;
             float resultado;
-            int choiseUser;
+            int choiseUser = -1;
 
             Console.WriteLine ("Bem vindo a calculadora simples");
+
+            while (choiseUser != 0) {
+                MostrarOpcoes ();
+
+                choiseUser = int.Parse (Console.ReadLine ());
+
+                switch (choiseUser) {
+                    case 1:
+
+                        Console.WriteLine ("Digite o primeiro numero:");
+                        num1 = float.Parse (Console.ReadLine ());
+                        Console.WriteLine ("Digite o segundo numero:");
+                        num2 = float.Parse (Console.ReadLine ());
+                        resultado = num1 + num2;
+
+                        Console.WriteLine ($"{num1} + {num2} = {resultado}");
+                        break;
+                    case 2:
+
+                        Console.WriteLine ("Digite o primeiro numero:");
+                        num1 = float.Parse (Console.ReadLine ());
+                        Console.WriteLine ("Digite o segundo numero:");
+                        num2 = float.Parse (Console.ReadLine ());
+                        resultado = num1 - num2;
+                        Console.WriteLine ($"{num1} - {num2} = {resultado}");
+                        break;
+                    case 3:
+
+                        Console.WriteLine ("Digite o primeiro numero:");
+                        num1 = float.Parse (Console.ReadLine ());
+                        Console.WriteLine ("Digite o segundo numero:");
+                        num2 = float.Parse (Console.ReadLine ());
+                        resultado = num1 * num2;
+                        Console.WriteLine ($"{num1} * {num2} = {resultado}");
+                        break;
+                    case 4:
+
+                        Console.WriteLine ("Digite o primeiro numero:");
+                        num1 = float.Parse (Console.ReadLine ());
+                        Console.WriteLine ("Digite o segundo numero:");
+                        num2 = float.Parse (Console.ReadLine ());
+                        resultado = num1 / num2;
+                        Console.WriteLine ($"{num1} / {num2} = {resultado}");
+                        break;
+                    case 0:
+                        Console.WriteLine ("Obrigado por usar a calculadora!\n");
+                        break;
+                    default:
+                        Console.WriteLine ("Opção não encontrada!\n");
+                        break;
+                }
+
+            }
+        }
+        public static void MostrarOpcoes () {
+
             Console.WriteLine ("1 - Para somar(+)");
             Console.WriteLine ("2 - Para diminuir(-)");
             Console.WriteLine ("3 - Para multiplicar(*)");
             Console.WriteLine ("4 - Para dividir(/)");
-
-            choiseUser = int.Parse (Console.ReadLine ());
-
-            switch (choiseUser) {
-                case 1:
-
-                    Console.WriteLine ("Digite o primeiro numero:");
-                    num1 = float.Parse (Console.ReadLine ());
-                    Console.WriteLine ("Digite o segundo numero:");
-                    num2 = float.Parse (Console.ReadLine ());
-                    resultado = num1 + num2;
-
-                    Console.WriteLine (resultado);
-                    break;
-                case 2:
-
-                    Console.WriteLine ("Digite o primeiro numero:");
-                    num1 = float.Parse (Console.ReadLine ());
-                    Console.WriteLine ("Digite o segundo numero:");
-                    num2 = float.Parse (Console.ReadLine ());
-                    resultado = num1 - num2;
-                    Console.WriteLine (resultado);
-                    break;
-                case 3:
-
-                    Console.WriteLine ("Digite o primeiro numero:");
-                    num1 = float.Parse (Console.ReadLine ());
-                    Console.WriteLine ("Digite o segundo numero:");
-                    num2 = float.Parse (Console.ReadLine ());
-                    resultado = num1 * num2;
-                    Console.WriteLine (resultado);
-                    break;
-                case 4:
-
-                    Console.WriteLine ("Digite o primeiro numero:");
-                    num1 = float.Parse (Console.ReadLine ());
-                    Console.WriteLine ("Digite o segundo numero:");
-                    num2 = float.Parse (Console.ReadLine ());
-                    resultado = num1 / num2;
-                    Console.WriteLine (resultado);
-                    break;
-                default:
-                    throw new InvalidOperationException ("Opção não encontrada");
-
-            }
-
+            Console.WriteLine ("0 - Para encerrar o programa.");
         }
 
     }
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 CalcGithub
-Program.cs - switch case.
